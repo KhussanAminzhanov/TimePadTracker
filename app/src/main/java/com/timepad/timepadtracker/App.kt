dbc93beebf3dc07b817ee4b26864f8f7271d6784
Khussan Aminzhanov <aminjanovhusan07@gmail.com>
2022-07-20 09:25:01 +0600

    add dependencies

---
+++ app/src/main/java/com/timepad/timepadtracker/App.kt
@@ -0,0 +1,18 @@
+package com.timepad.timepadtracker
+
+import android.app.Application
+import com.timepad.timepadtracker.di.mainModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(mainModule)
+        }
+    }
+}
