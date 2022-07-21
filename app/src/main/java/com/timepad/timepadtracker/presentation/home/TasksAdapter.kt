6d9911653dea71c34fc025e27670e6a46c59614b
Daniyar Yergesh <dancho.ktl@gmail.com>
2022-07-21 12:56:34 +0600

    modify xml of task fragment

--- app/src/main/java/com/timepad/timepadtracker/presentation/home/TasksAdapter.kt
+++ app/src/main/java/com/timepad/timepadtracker/presentation/home/TasksAdapter.kt
@@ -5,27 +5,27 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.timepad.timepadtracker.databinding.ItemTasksRvBinding
+import com.timepad.timepadtracker.databinding.ItemTasksBinding
 import com.timepad.timepadtracker.domain.Task
 
 class TasksAdapter : ListAdapter<Task, TasksAdapter.TasksViewHolder>(TasksDiffUtilCallback()) {
 
-    inner class TasksViewHolder(val binding: ItemTasksRvBinding) :
+    inner class TasksViewHolder(val binding: ItemTasksBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: Task) {
             binding.ivTaskIcon.setImageResource(item.iconId)
             binding.tvTaskTitle.text = item.name
             binding.tvTimeSpent.text = item.totalTimeInMillis.toString()
-            binding.tvFirstCategory.text = item.tags[0]
-            binding.tvSecondCategory.text = item.tags[1]
+            binding.tvFirstCategory.text = "Personal"
+            binding.tvSecondCategory.text = "Sport"
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
         return TasksViewHolder(
-            ItemTasksRvBinding.inflate(
+            ItemTasksBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
