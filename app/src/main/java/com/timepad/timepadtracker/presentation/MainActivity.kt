3b7b1b386bfeda6b64c5bed3fb480bb30cd81432
Khussan Aminzhanov <aminjanovhusan07@gmail.com>
2022-07-22 13:01:14 +0600

    add main fragment navigation and remove toolbar and bottom navbar from activity

--- app/src/main/java/com/timepad/timepadtracker/presentation/MainActivity.kt
+++ app/src/main/java/com/timepad/timepadtracker/presentation/MainActivity.kt
@@ -5,15 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationItemView
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.timepad.timepadtracker.R
 import com.timepad.timepadtracker.databinding.ActivityMainBinding
-import com.timepad.timepadtracker.domain.Task
-import com.timepad.timepadtracker.presentation.home.HomeViewModel
-import com.timepad.timepadtracker.utils.dp
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,9 +14,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
-
-    private val homeViewModel: HomeViewModel by viewModel()
-    private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,26 +24,12 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         setupNavigation()
-        setupBottomNav()
     }
 
     private fun setupNavigation() {
         navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment_container_main) as NavHostFragment
+            .findFragmentById(R.id.nav_host_fragment_container_activity_main) as NavHostFragment
         navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration.Builder(navController.graph).build()
-
-        binding.toolbarMain.setupWithNavController(navController, appBarConfiguration)
-        binding.bottomNavMain.setupWithNavController(navController)
-    }
-
-    private fun setupBottomNav() {
-        val menuView = binding.bottomNavMain.menuView as BottomNavigationMenuView
-        val menuItem = menuView.findItemView(R.id.item_add_task) as BottomNavigationItemView
-        menuItem.setIconSize(44.dp)
-        menuItem.setOnClickListener {
-            val tags = listOf("Work", "Study")
-            homeViewModel.addTask(Task(0, 0, "Task ${counter++}", tags, 0))
-        }
     }
 }
--- app/src/main/java/com/timepad/timepadtracker/presentation/main/MainFragment.kt
+++ app/src/main/java/com/timepad/timepadtracker/presentation/main/MainFragment.kt
@@ -1,8 +1,71 @@
 package com.timepad.timepadtracker.presentation.main
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.timepad.timepadtracker.R
+import com.timepad.timepadtracker.databinding.FragmentMainBinding
+import com.timepad.timepadtracker.domain.Task
+import com.timepad.timepadtracker.presentation.home.HomeViewModel
+import com.timepad.timepadtracker.utils.dp
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
 
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private val homeViewModel: HomeViewModel by viewModel()
+    private var counter = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupNavigation()
+        setupBottomNav()
+    }
+
+    private fun setupNavigation() {
+        navHostFragment =
+            childFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration.Builder(navController.graph).build()
+
+        binding.bottomNavMain.setupWithNavController(navController)
+        binding.toolbarMain.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun setupBottomNav() {
+        val menuView = binding.bottomNavMain.menuView as BottomNavigationMenuView
+        val menuItem = menuView.findItemView(R.id.item_add_task) as BottomNavigationItemView
+        menuItem.setIconSize(44.dp)
+        menuItem.setOnClickListener {
+            val tags = listOf("Work", "Study")
+            homeViewModel.addTask(Task(0, 0, "Task ${counter++}", tags, 0))
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
