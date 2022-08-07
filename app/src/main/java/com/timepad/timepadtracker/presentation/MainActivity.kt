aa52b210c42cbca6cd9316b142306ef4c6db1366
Khussan Aminzhanov <46312533+KhussanAminzhanov@users.noreply.github.com>
2022-08-07 11:39:28 +0600

    delete fragment and adapter

--- app/src/main/java/com/timepad/timepadtracker/presentation/MainActivity.kt
+++ app/src/main/java/com/timepad/timepadtracker/presentation/MainActivity.kt
@@ -3,7 +3,10 @@ package com.timepad.timepadtracker.presentation
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.runtime.CompositionLocalProvider
 import com.timepad.timepadtracker.presentation.navigation.Navigation
+import com.timepad.timepadtracker.presentation.screens.main.NoRippleTheme
 import com.timepad.timepadtracker.presentation.theme.TimePadTheme
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TimePadTheme {
-                Navigation()
+                CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
+                    Navigation()
+                }
             }
         }
     }
